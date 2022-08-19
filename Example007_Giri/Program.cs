afad063dbcc565cd323dfe418cc3fa41b1aa11c3
Sergey <optimtag@mail.ru>
2022-08-19 16:46:38 +0300

    Example007_Giri

---
+++ Example007_Giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 11;
+int b = 2;
+int c = 6;
+int d = 82;
+int e = 41;
+
+int max = a;
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+if ( d > max ) max = d;
+if ( e > max ) max = e;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
+
